7ec3d8f5e7b784ed086b829366b30d769307dda7
neko <suzumiya_sos@126.com>
2022-07-15 01:33:37 +0800

    add

--- packages/tenant/src/pages/scene/components/Modal/index.tsx
+++ packages/tenant/src/pages/scene/components/Modal/index.tsx
@@ -1,36 +1,68 @@
-import { useGLTF } from '@react-three/drei'
-import { useEffect, useRef } from 'react'
-import { Select } from '@react-three/postprocessing'
+import { useCubeTexture, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+import { Select } from "@react-three/postprocessing";
+import { Group, Material, MeshStandardMaterial } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useFrame, useLoader } from "@react-three/fiber";
 
-type onLoad = (gltf: any) => void
+type onLoad = (gltf: any) => void;
 type select = {
-  [index:string]:{value:boolean}
-}
-export default function Model({ path, onLoad,select, ...restProps }: { path: string, onLoad?: onLoad,select?:select }) {
-  // const group = useRef()
-  // const gltf = useLoader(GLTFLoader, '/Poimandres.gltf')
-  const group = useRef(null)
+  [index: string]: { value: boolean };
+};
+type ModelProps = {
+  path: string;
+  onLoad: onLoad;
+  select: select;
+};
 
-  let gltf = useGLTF(path)
+const material = () => (
+  <meshStandardMaterial
+    envMap={useCubeTexture(["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"], { path: "cube/" })}
+    roughness={0}
+    metalness={0.9}
+    color="#010101"
+  />
+);
+const Model = (props: ModelProps) => {
+  const { path, onLoad, select, ...restProps } = props;
+  const group = useRef<Group>(null);
+  let gltf = useGLTF(path) as any;
+  const envMap = useCubeTexture(["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"], { path: "cube/" });
+  useFrame(({ clock }) => {
+    if (!group.current) {
+      return;
+    }
+    // group.current.position.y > 50 ? group.current.position.y-- : group.current.position.y++;
+    // = clock.getElapsedTime() + 2;
+    console.log(clock.getDelta());
+    console.log(clock.getElapsedTime());
+    group.current.rotateY(1 / (2 * 60));
+  });
   // console.log(gltf)
   useEffect(() => {
-    gltf = useGLTF(path)
+    gltf = useGLTF(path) as any;
     if (onLoad) {
-      onLoad(gltf)
+      onLoad(gltf);
     }
-  }, [path])
+  }, [path]);
 
   const { nodes } = gltf;
   return (
-
     <group ref={group} {...restProps} dispose={null}>
-      {
-        gltf && Object.keys(nodes)
-          .map((key) =>
-            <Select  key={key} name={key} enabled={select?.[key]?.value}>
-              <mesh geometry={nodes[key].geometry} material={nodes[key].material} />
-            </Select>)
-      }
+      {gltf &&
+        Object.keys(nodes).map((key) => (
+          <Select key={key} name={key} enabled={select?.[key]?.value}>
+            <mesh
+              geometry={nodes[key].geometry}
+              material={
+                new MeshStandardMaterial({
+                  color: "#000",
+                })
+              }
+            />
+          </Select>
+        ))}
     </group>
-  )
-}
+  );
+};
+export default Model;
--- packages/tenant/src/pages/scene/index.tsx
+++ packages/tenant/src/pages/scene/index.tsx
@@ -1,13 +1,21 @@
 import { Suspense, useMemo, useRef, useState } from "react";
 import * as THREE from "three";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Bounds, ContactShadows, Environment, GizmoHelper, GizmoViewport, OrbitControls } from "@react-three/drei";
-import { Selection, EffectComposer, Outline, SSAO, SMAA } from '@react-three/postprocessing'
+import {
+  Bounds,
+  ContactShadows,
+  Environment,
+  GizmoHelper,
+  GizmoViewport,
+  OrbitControls,
+  Stats,
+} from "@react-three/drei";
+import { Selection, EffectComposer, Outline, SSAO, SMAA, Scanline, Bloom } from "@react-three/postprocessing";
 import { AddFour, ArrowLeft, HamburgerButton, PlayOne, ShareOne, UploadOne, Anchor, Down } from "@icon-park/react";
 import { Select, Space, Tabs, Tree, Upload } from "antd";
-import type { DataNode } from 'antd/es/tree';
+import type { DataNode } from "antd/es/tree";
 import { RcFile } from "antd/lib/upload/interface";
-import { useWindowSize } from 'react-use';
+import { useWindowSize } from "react-use";
 import Loader from "./components/Loader";
 import Model from "./components/Modal";
 import SideBar from "./components/SideBar";
@@ -15,49 +23,52 @@ import style from "./index.module.less";
 import ToolBar from "./components/ToolBar";
 import PropertyPane from "./components/PropertyPane";
 import MaterialList from "./components/Material";
-import Text from './components/Text'
+import Text from "./components/Text";
 import Sparks from "./components/Sparks";
 import Particles from "./components/Particles";
 import Effects from "./components/Effects";
-
+import { Group, Mesh } from "three";
 
 const { TabPane } = Tabs;
 const { Option } = Select;
 
 // sunset, dawn, night, warehouse, forest, apartment, studio, city, park, lobby
-const envPresets = [{
-  name: "city",
-  path: "/potsdamer_platz_1k.hdr"
-}, {
-  name: "sunset",
-  path: "/venice_sunset_1k.hdr"
-}, {
-  name: "studio",
-  path: "/studio_small_03_1k.hdr"
-},
-{
-  name: "night",
-  path: "/dikhololo_night_1k.hdr"
-}
-]
+const envPresets = [
+  {
+    name: "city",
+    path: "/potsdamer_platz_1k.hdr",
+  },
+  {
+    name: "sunset",
+    path: "/venice_sunset_1k.hdr",
+  },
+  {
+    name: "studio",
+    path: "/studio_small_03_1k.hdr",
+  },
+  {
+    name: "night",
+    path: "/dikhololo_night_1k.hdr",
+  },
+];
 
 type TreeData = {
   name: string;
   children: TreeData[];
   uuid: string;
-}
+};
 
 function Ellipse(props) {
   const geometry = useMemo(() => {
-    const curve = new THREE.EllipseCurve(0, 0, 10, 3, 0, 2 * Math.PI, false, 0)
-    const points = curve.getPoints(50)
-    return new THREE.BufferGeometry().setFromPoints(points)
-  }, [])
+    const curve = new THREE.EllipseCurve(0, 0, 10, 3, 0, 2 * Math.PI, false, 0);
+    const points = curve.getPoints(50);
+    return new THREE.BufferGeometry().setFromPoints(points);
+  }, []);
   return (
     <line geometry={geometry} {...props}>
       <meshBasicMaterial />
     </line>
-  )
+  );
 }
 function ReactAtom(props) {
   return (
@@ -70,40 +81,44 @@ function ReactAtom(props) {
         <meshBasicMaterial color="red" />
       </mesh>
     </group>
-  )
+  );
 }
 function Number({ hover }) {
-  const ref = useRef()
+  const ref = useRef();
   useFrame((state) => {
     if (ref.current) {
-      ref.current.position.x = THREE.MathUtils.lerp(ref.current.position.x, state.mouse.x * 2, 0.1)
-      ref.current.rotation.x = THREE.MathUtils.lerp(ref.current.rotation.x, state.mouse.y / 2, 0.1)
-      ref.current.rotation.y = 0.8
+      ref.current.position.x = THREE.MathUtils.lerp(ref.current.position.x, state.mouse.x * 2, 0.1);
+      ref.current.rotation.x = THREE.MathUtils.lerp(ref.current.rotation.x, state.mouse.y / 2, 0.1);
+      ref.current.rotation.y = 0.8;
     }
-  })
+  });
   return (
     <Suspense fallback={null}>
       <group ref={ref}>
         <Text
           size={10}
-          onClick={(e) => window.open('https://github.com/react-spring/react-three-fiber/blob/master/whatsnew.md', '_blank')}
+          onClick={(e) =>
+            window.open("https://github.com/react-spring/react-three-fiber/blob/master/whatsnew.md", "_blank")
+          }
           onPointerOver={() => hover(true)}
-          onPointerOut={() => hover(false)}>
+          onPointerOut={() => hover(false)}
+        >
           4
         </Text>
         <ReactAtom position={[35, -20, 0]} scale={[1, 0.5, 1]} />
       </group>
     </Suspense>
-  )
+  );
 }
 const Scene = () => {
-  const [hovered, hover] = useState(false)
-  const mouse = useRef([0, 0])
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+  const [hovered, hover] = useState(false);
+  const mouse = useRef([0, 0]);
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   const [modelPath, setModelPath] = useState("");
-  const [treeData, setTreeData] = useState<DataNode[]>([])
+  const [treeData, setTreeData] = useState<DataNode[]>([]);
   // const [hovered, hover] = useState(null)
-  const [env, setEnv] = useState("/potsdamer_platz_1k.hdr")
+  const [env, setEnv] = useState("/potsdamer_platz_1k.hdr");
+
   const { width, height } = useWindowSize();
   const [selectConfig, setSelectConfig] = useState({
     shoe: { value: false },
@@ -114,7 +129,7 @@ const Scene = () => {
     shoe_5: { value: false },
     shoe_6: { value: false },
     shoe_7: { value: false },
-  },)
+  });
   //  const config = useControls({
   //   all: { value: false },
   //   parts: folder(
@@ -131,44 +146,48 @@ const Scene = () => {
   //     { collapsed: true },
   //   ),
   // })
+
   const onUploadModal = (file: RcFile) => {
-    const path = window.URL.createObjectURL(file)
-    setModelPath(path)
-  }
-  const onChangeTab = () => { };
+    const path = window.URL.createObjectURL(file);
+    setModelPath(path);
+  };
+  const onChangeTab = () => {};
 
   const onChangeEnv = (value) => {
     console.log(value);
-    setEnv(value)
-  }
+    setEnv(value);
+  };
 
   const onLoadModel = (model) => {
     console.log(model.nodes);
-    const nodes = model.nodes
-    const { name, children, uuid } = nodes['Scene']
-    setTreeData([{
-      name, children, uuid
-    }]);
-  }
+    const nodes = model.nodes;
+    const { name, children, uuid } = nodes["Scene"];
+    setTreeData([
+      {
+        name,
+        children,
+        uuid,
+      },
+    ]);
+  };
   const onSelectTree = (selectedKeys, e) => {
     console.log(e);
-    const { node, selectedNodes } = e
-    const name = node.name
+    const { node, selectedNodes } = e;
+    const name = node.name;
     console.log(name);
     Object.keys(selectConfig).forEach((key) => {
       selectConfig[key] = false;
-    })
+    });
     selectedNodes.forEach((node) => {
       const name = node.name;
-      setSelectConfig(pre => ({
+      setSelectConfig((pre) => ({
         ...pre,
         [name]: {
-          value: true
-        }
-      }))
-    })
-
-  }
+          value: true,
+        },
+      }));
+    });
+  };
   return (
     <div className="scene">
       <ToolBar>
@@ -205,12 +224,10 @@ const Scene = () => {
             blockNode
             multiple
             switcherIcon={<Down theme="outline" size="18" fill="#333" />}
-            defaultExpandedKeys={['0-0-0']}
+            defaultExpandedKeys={["0-0-0"]}
             onSelect={onSelectTree}
             treeData={treeData}
-            fieldNames={
-              { title: "name", key: "uuid", }
-            }
+            fieldNames={{ title: "name", key: "uuid" }}
           />
         </div>
       </SideBar>
@@ -222,35 +239,43 @@ const Scene = () => {
           background: "#000",
         }}
         onCreated={({ gl }) => {
-          gl.toneMapping = THREE.Uncharted2ToneMapping
-          gl.setClearColor(new THREE.Color('#020207'))
+          gl.toneMapping = THREE.ACESFilmicToneMapping;
+          gl.setClearColor(new THREE.Color("#020207"));
         }}
       >
-        <fog attach="fog" args={['white', 50, 190]} />
+        <Stats showPanel={0} className="stats" />
+        <Stats showPanel={2} className="stats" />
+        {/* <fog attach="fog" args={['white', 50, 190]} />
         <pointLight distance={100} intensity={4} color="white" />
         <Number mouse={mouse} hover={hover} />
         <Particles count={isMobile ? 5000 : 10000} mouse={mouse} />
         <Sparks count={20} mouse={mouse} colors={['#A2CCB6', '#FCEEB5', '#EE786E', '#e0feff', 'lightpink', 'lightblue']} />
-        <Effects />
+        <Effects /> */}
         {/* <ambientLight intensity={0.7} /> */}
         {/* <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow /> */}
         <Suspense fallback={<Loader />}>
+          <EffectComposer multisampling={0} autoClear={false}>
+            <SSAO radius={0.05} intensity={30} luminanceInfluence={0.5} color="black" />
+            <Outline edgeStrength={100} visibleEdgeColor={31743} hiddenEdgeColor={21743} blur />
+            <SMAA />
+            {/* <Bloom
+              intensity={300} // The bloom intensity. // A blur pass.
+              luminanceThreshold={0.9} // luminance threshold. Raise this value to mask out darker elements in the scene.
+              luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+            /> */}
+          </EffectComposer>
           <Selection>
-            <EffectComposer multisampling={0} autoClear={false}>
-              {/* <SSAO radius={0.05} intensity={30} luminanceInfluence={0.5} color="black" /> */}
-              <Outline edgeStrength={100} visibleEdgeColor={31743} hiddenEdgeColor={21743} blur />
-              {/* <SMAA /> */}
-            </EffectComposer>
-            {
-              modelPath.length > 0 ? <Model path={modelPath} onLoad={onLoadModel} select={selectConfig} /> : <></>
-            }
+            {modelPath.length > 0 ? (
+              <Model path={modelPath} onLoad={onLoadModel} select={selectConfig} />
+            ) : (
+              <></>
+            )}
             {/* <Bounds fit clip margin={1.2} damping={0}>
               {
                 modelPath.length > 0 ? <Model path={modelPath} onLoad={ onLoadModel } /> : <></>
               }
             </Bounds> */}
           </Selection>
-
           <Environment files={env} />
           <ContactShadows position={[0, -0.8, 0]} opacity={0.25} scale={10} blur={1.5} far={0.8} />
         </Suspense>
@@ -263,10 +288,13 @@ const Scene = () => {
         <div className={style.property}>
           <Tabs defaultActiveKey="1" onChange={onChangeTab} type="card" tabPosition="left">
             <TabPane tab="环境" key="1">
-              环境光：<Select defaultValue="city" style={{ width: 120 }} onChange={onChangeEnv}>
-                {
-                  envPresets.map((env, idx) => <Option key={idx} value={env.path}>{env.name}</Option>)
-                }
+              环境光：
+              <Select defaultValue="city" style={{ width: 120 }} onChange={onChangeEnv}>
+                {envPresets.map((env, idx) => (
+                  <Option key={idx} value={env.path}>
+                    {env.name}
+                  </Option>
+                ))}
               </Select>
             </TabPane>
             <TabPane tab="材质" key="2">
